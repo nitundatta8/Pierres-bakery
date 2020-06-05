d0c72ca95d1901d5712908361d32ad2db699d8f0
nitunwa <nitun.2@gmail.com>
2020-06-05 11:06:31 -0700

    successfully navigate to the home page

--- PierresBakery/Models/Flavor.cs
+++ PierresBakery/Models/Flavor.cs
@@ -8,6 +8,7 @@ namespace PierresBakery.Models
     public string Name { get; set; }
     public string Description { get; set; }
     public ICollection<FlavorTreat> Treats { get; set; }
+    public virtual ApplicationUser User { get; set; }
     public Flavor()
     {
       this.Treats = new HashSet<FlavorTreat>();
