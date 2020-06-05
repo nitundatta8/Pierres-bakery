55ea45044eeea67294e3817c5cbd95111a0228da
nitunwa <nitun.2@gmail.com>
2020-06-05 15:10:06 -0700

    add Authorization to database migration and functionality to display and create treats

--- PierresBakery/Controllers/TreatsController.cs
+++ PierresBakery/Controllers/TreatsController.cs
@@ -7,20 +7,47 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using System.Threading.Tasks;
 using System.Security.Claims;
-
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace PierresBakery.Controllers
 {
+  [Authorize]
   public class TreatsController : Controller
   {
     private readonly PierresBakeryContext _db;
-    private readonly UserManager<ApplicationUser> _userManager; //new line
+    private readonly UserManager<ApplicationUser> _userManager;
 
-    //updated constructor
+
     public TreatsController(UserManager<ApplicationUser> userManager, PierresBakeryContext db)
     {
       _userManager = userManager;
       _db = db;
+    }
+    public async Task<ActionResult> Index()
+    {
+      var userId = this.User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+      var currentUser = await _userManager.FindByIdAsync(userId);
+      var userItems = _db.Treats.Where(entry => entry.User.Id == currentUser.Id);
+      return View(userItems);
+    }
+    public ActionResult Create()
+    {
+      ViewBag.FlavorId = new SelectList(_db.Flavors, "FlavorId", "Name");
+      return View();
+    }
+    [HttpPost]
+    public async Task<ActionResult> Create(Treat treat, int FlavourId)
+    {
+      var userId = this.User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+      var currentUser = await _userManager.FindByIdAsync(userId);
+      treat.User = currentUser;
+      _db.Treats.Add(treat);
+      if (FlavourId != 0)
+      {
+        _db.FlavorTreats.Add(new FlavorTreat() { FlavorId = FlavourId, TreatId = treat.TreatId });
+      }
+      _db.SaveChanges();
+      return RedirectToAction("Index");
     }
   }
 }
--- PierresBakery/Models/Treat.cs
+++ PierresBakery/Models/Treat.cs
@@ -8,6 +8,7 @@ namespace PierresBakery.Models
     public string Name { get; set; }
     public string Description { get; set; }
     public ICollection<FlavorTreat> Flavors { get; set; }
+    public virtual ApplicationUser User { get; set; }
 
     public Treat()
     {
